8f462f43895b7cf52a553f32d7fdf3e2f64f10fe
Ajay Pal <ajay.pec24@gmail.com>
2022-10-11 16:09:47 +0530

    Made appropriate changes to make the timer to target a specifi time (New year in this case)

--- js/master.js
+++ js/master.js
@@ -1,13 +1,18 @@
+var targetDate
 $(document).ready(function () {
+  targetDate=new Date('01 Jan 2022 00:00:00');
+  targetDate.setFullYear(new Date().getFullYear()+1);
+
+  console.log(targetDate)
   $(".cal-container").each(function(){
-    ivl= $(this).attr("data-interval")
-    spoint= $(this).attr("data-start")
-    value= $(this).attr("data-value")
-    call(this, spoint, ivl, value)
+    period= $(this).attr("data-period")
+    addCounterCardsHtml(this);
+    call(this, period);
+
   })
 });
 
-function call(item,number,interval, value=none) {
+function addCounterCardsHtml(item){
   let newHtml=`<div class="cal-box old top">+
     <div class="number"></div>
   </div>
@@ -20,43 +25,62 @@ function call(item,number,interval, value=none) {
   <div class="cal-box new bottom">
         <div class="number"></div>
   </div>`;
-  $(item).html(newHtml)
-
-
-  $(item).children(".cal-container > .old").text(number);
-  let num = number - 1;
-  $(item).children(".cal-container > .new").text(num);
-  console.log($(item).children(".cal-container > .new"))
-  setInterval(function () {
-    addimpclasses(item, num, number, value);
-    num -= 1;
-    if (num == -1) {
-      num = value;
-    }
-  }, interval*1000);
+  $(item).html(newHtml);
 }
-function addimpclasses(item, num, number) {
+
+function call(item,period) {
+
+  $(item).children(".cal-container > .old").text(00);
+  $(item).children(".cal-container > .new").text(00);
+  // console.log($(item).children(".cal-container > .new"))
+  setInterval(function () {
+
+    let remainingTime=targetDate-new Date()
+
+    remainingDays=parseInt(remainingTime/86400000);
+    remainingTime %=86400000;
+    remainingHours=parseInt(remainingTime/3600000);
+    remainingTime %=3600000;
+    remainingMinutes=parseInt(remainingTime/60000);
+    remainingTime %=60000;
+    remainingSeconds=parseInt(remainingTime/1000);
+    data={"days":remainingDays, "hours":remainingHours, "minutes":remainingMinutes,"seconds":remainingSeconds}
+    addimpclasses(item, data[period]);
+  }, 1000);
+}
+function addimpclasses(item,  newNumber) {
+  let oldCard=$(item).children(".cal-container > .old");
+  let newCard=$(item).children(".cal-container > .new");
+  // Do not take any action if this item has not chnaged
+  if (newNumber==parseInt($(oldCard[0]).text()))
+  {
+    return
+  }
   let oldBottom= $(item).children(".cal-container > .old.bottom").eq(0);
   let newBottom= $(item).children(".cal-container > .new.bottom").eq(0);
   let oldTop= $(item).children(".cal-container > .old.top").eq(0);
   let newTop= $(item).children(".cal-container > .new.top").eq(0);
-  let oldCard=$(item).children(".cal-container > .old");
-  let newCard=$(item).children(".cal-container > .new");
 
 
+  newCard.text((newNumber).toString().padStart(2,0));
+
+  // Play bottom part card animation
   oldBottom.addClass("anim-old-bottom");
+
+  // Play top part card animations after 400s when above animation is complete
   setTimeout(function () {
     oldTop.addClass("anim-old-top");
     newTop.addClass("anim-new-top");
   }, 400);
-  // $(".old").text(num);
-  // $(".new").text(num-1);
+
+
+  // Change old Card and remove animations after 900s when animations are complete
   setTimeout(function () {
-    oldCard.text(num.toString().padStart(2,0));
-    if (num == 0) {
-      num = parseInt(value);
-    }
-   newCard.text((num - 1).toString().padStart(2,0));
+    oldCard.text(newNumber.toString().padStart(2,0));
+   //  if (num == 0) {
+   //    num = parseInt(value);
+   //  }
+   // newCard.text((num - 1).toString().padStart(2,0));
     oldBottom.removeClass("anim-old-bottom");
     oldTop.removeClass("anim-old-top");
     newTop.removeClass("anim-new-top");
